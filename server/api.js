6fc3a2f74777e6ccfeb8e8b9c813f9d4e57e9dfa
Victor CAZAUX <victor.cazaux@edu.devinci.fr>
2022-03-14 15:25:53 +0100

    feat(get-product): api+

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const produits =require('./all_products.json')
 
 const PORT = 8092;
 
@@ -14,9 +15,20 @@ app.use(helmet());
 
 app.options('*', cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+app.get('/price/:id', (request, response) => {
+  const idprice = parseInt(request.params.id)
+  const price = produits.filter(produit => produit.price === idprice)
+  response.status(200).json(price)
+
 });
+
+app.get('/brand/:id', (request, response) => {
+  const idbrand = request.params.id
+  const marque = produits.filter(produit => produit.brand === idbrand)
+  response.status(200).json(marque)
+
+});
+
 
 app.listen(PORT);
 
